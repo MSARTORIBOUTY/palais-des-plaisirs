5676eade786b16bd74364ba5708fd214b3b2b35c
MSARTORIBOUTY <marielaure.sartoribouty@ynov.com>
2022-05-19 00:38:54 +0200

    contact page

--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -6,14 +6,22 @@ use App\Entity\Message;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class MessageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('content')
+            ->add('email', TextType::class, [
+                'label' => 'Email',
+                'attr' => ['class' => 'input-contact' ],
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Message',
+                'attr' => ['class' => 'input-contact', 'rows' => '10' ],
+            ]);
         ;
     }
 
