6771321f91e846505b25aaf487bd5e9441ae379a
Valentin <chico030372@gmail.com>
2022-05-23 14:39:31 +0200

    Back

---
+++ src/Form/ArticlesType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Articles;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticlesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'attr' => ['class' => 'input-article' ],
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu',
+                'attr' => ['class' => 'input-article', 'rows' => '10' ],
+            ])
+            ->add('picture', FileType::class, [
+                'label' => 'Titre',
+                'attr' => ['class' => 'input-article' ],
+            ])
+            ->add('id_categorie', CategoriesType::class, [
+                'label' => 'Catégorie',
+                'attr' => ['class' => 'fileType']
+            ])
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Articles::class,
+        ]);
+    }
+}
